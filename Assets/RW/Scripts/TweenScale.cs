e7299a33eb81d6519d7aff976749b75a4dd5ac02
rebollocarla <carla.rebollo01@estudiant.upf.edu>
2023-05-01 19:47:08 +0200

    sheep rescue

---
+++ Assets/RW/Scripts/TweenScale.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TweenScale : MonoBehaviour
+{
+    public float targetScale;
+    public float timeToReachTarget;
+    private float startScale;
+    private float percentScaled;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        startScale = transform.localScale.x;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (percentScaled < 1f) // 1
+        {
+        percentScaled += Time.deltaTime / timeToReachTarget; // 2
+        float scale = Mathf.Lerp(startScale, targetScale, percentScaled); // 3
+        transform.localScale = new Vector3(scale, scale, scale); // 4
+        }   
+    }
+}
